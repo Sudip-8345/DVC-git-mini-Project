7655ff57f197597fdfbb0c398f182958bea3f948
Sudip <sudipd2004@gmail.com>
2025-06-04 23:22:33 +0530

    third changes are alo done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -5,9 +5,9 @@ import os
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv')
 
 df = df.iloc[:,3:]
-df = df[df['Length of Membership']>3]
+df = df[df['Length of Membership']>5]
 
-df.drop(columns=['Avg. Session Length'],inplace=True)
+df.drop(columns=['Avg. Session Length','Time on Website'],inplace=True)
 
 df.to_csv(os.path.join('data','customer_data.csv'), index=False)
 
